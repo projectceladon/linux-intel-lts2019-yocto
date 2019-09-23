4d41943ac94ee14ffe3ae8d1635313e78bc0bb8c
he, bo <bo.he@intel.com>
2019-09-23 08:24:24 +0800

    ion: fix the tons of warning from PAT
    we see tons of warning like:
    [   45.846872] x86/PAT: NDK MediaCodec_:3753 map pfn RAM range req
    write-combining for [mem 0x1e7a80000-0x1e7a87fff], got write-back
    [   45.848827] x86/PAT: .vorbis.decoder:4088 map pfn RAM range req
    write-combining for [mem 0x1e7a58000-0x1e7a58fff], got write-back
    [   45.848875] x86/PAT: NDK MediaCodec_:3753 map pfn RAM range req
    write-combining for [mem 0x1e7a48000-0x1e7a4ffff], got write-back
    [   45.849403] x86/PAT: .vorbis.decoder:4088 map pfn RAM range
    req write-combining for [mem 0x1e7a70000-0x1e7a70fff], got write-back
    
    check the kernel Documentation/x86/pat.txt, it says:
    A. Exporting pages to users with remap_pfn_range, io_remap_pfn_range,
    vm_insert_pfn
    Drivers wanting to export some pages to userspace do it by using
    mmap interface and a combination of
    1) pgprot_noncached()
    2) io_remap_pfn_range() or remap_pfn_range() or vm_insert_pfn()
    With PAT support, a new API pgprot_writecombine is being added.
    So, drivers can continue to use the above sequence, with either
    pgprot_noncached() or pgprot_writecombine() in step 1, followed by step 2.
    
    In addition, step 2 internally tracks the region as UC or WC in
    memtype list in order to ensure no conflicting mapping.
    
    Note that this set of APIs only works with IO (non RAM) regions.
    If driver ants to export a RAM region, it has to do set_memory_uc() or
    set_memory_wc() as step 0 above and also track the usage of those pages and use
    set_memory_wb() before the page is freed to free pool.
    
    the fix follow the pat document, do set_memory_wc() as step 0 and
    use the set_memory_wb() before the page is freed.
    
    Tracked-On: PKT-2687
    Signed-off-by: he, bo <bo.he@intel.com>

--- drivers/staging/android/ion/ion_system_heap.c
+++ drivers/staging/android/ion/ion_system_heap.c
@@ -13,7 +13,7 @@
 #include <linux/scatterlist.h>
 #include <linux/slab.h>
 #include <linux/vmalloc.h>
-
+#include <asm/set_memory.h>
 #include "ion.h"
 #include "ion_page_pool.h"
 
@@ -317,6 +317,11 @@ static int ion_system_contig_heap_allocate(struct ion_heap *heap,
 
 	buffer->sg_table = table;
 
+#ifdef CONFIG_X86
+	if (!(buffer->flags & ION_FLAG_CACHED))
+		set_memory_wc((unsigned long)page_address(page), PAGE_ALIGN(len) >> PAGE_SHIFT);
+#endif
+
 	return 0;
 
 free_table:
@@ -334,6 +339,11 @@ static void ion_system_contig_heap_free(struct ion_buffer *buffer)
 	struct page *page = sg_page(table->sgl);
 	unsigned long pages = PAGE_ALIGN(buffer->size) >> PAGE_SHIFT;
 	unsigned long i;
+
+#ifdef CONFIG_X86
+	if (!(buffer->flags & ION_FLAG_CACHED))
+		set_memory_wb((unsigned long)page_address(page), pages);
+#endif
 
 	for (i = 0; i < pages; i++)
 		__free_page(page + i);
